8a1e8df63ea7e39c78e18f1363deeb16e70f9cad
Diana Olarte <diana.olarte@edunext.co>
2025-07-29 11:44:37 +1000

    feat: add the CorporatePartnerPage to the App Router

---
+++ src/partner/CorporatePartnerPage.tsx
@@ -0,0 +1,10 @@
+import AppLayout from '../app/AppLayout';
+import CorpotatePartnerList from './components/CorporatePartnerList';
+
+const CorpotatePartnerPage = () => (
+  <AppLayout title="Corporate Partners">
+    <CorpotatePartnerList />
+  </AppLayout>
+);
+
+export default CorpotatePartnerPage;
---
+++ src/partner/messages.js
@@ -0,0 +1,11 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  titleViewCourseIn: {
+    id: 'corporate.partner.page.title',
+    defaultMessage: 'Corporate Partner',
+    description: 'Title for the corportate partner page',
+  },
+});
+
+export default messages;
