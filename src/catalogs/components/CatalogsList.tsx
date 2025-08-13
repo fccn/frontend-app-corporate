af68766fa56fc434b5f70ca66a1dc88f992dad44
Brayan Ceron <brayan.ceron@edunext.co>
2025-08-13 16:14:20 -0500

    feat: add PartnerCatalogsPage and CatalogsList components with routing

---
+++ src/catalogs/components/CatalogsList.tsx
@@ -0,0 +1,106 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { useIntl } from '@edx/frontend-platform/i18n';
+import {
+  DataTable, TextFilter,
+} from '@openedx/paragon';
+
+import { CorporatePartner } from '@src/app/types';
+import TableName from '@src/app/TableName';
+import { useNavigate } from '@src/hooks';
+import { paths } from '@src/constants';
+import { getPartnerCatalogs } from '../api';
+import TableFooter from '../../app/TableFooter';
+
+import messages from '../messages';
+import ActionItem from '../../app/ActionItem';
+
+type CellValue = {
+  row: {
+    original: CorporatePartner;
+  }
+};
+
+const CatalogsList = () => {
+  const navigate = useNavigate();
+  const intl = useIntl();
+  const { data, isLoading } = useSuspenseQuery({
+    queryKey: ['partnerCatalogs'],
+    queryFn: () => getPartnerCatalogs(),
+  });
+
+  const tableActions = [{
+    type: 'view',
+    onClick: (partner: CorporatePartner) => navigate(paths.courses.buildPath(partner.code)),
+  }, {
+    type: 'edit',
+    onClick: (partner: CorporatePartner) => navigate(paths.catalogs.buildPath(partner.code)),
+  }];
+
+  return (
+    <DataTable
+      isLoading={isLoading}
+      isPaginated
+      isFilterable
+      defaultColumnValues={{ Filter: TextFilter }}
+      initialState={{
+        pageSize: 30,
+        pageIndex: 0,
+      }}
+      additionalColumns={[
+        {
+          id: 'action',
+          Header: intl.formatMessage(messages.headerAction),
+          Cell: ({ row }: CellValue) => tableActions.map(({ type, onClick }) => (
+            <ActionItem
+              key={`action-${type}-${row.original.code}`}
+              type={type}
+              onClick={() => onClick(row.original)}
+            />
+          )),
+        },
+      ]}
+      itemCount={data.length}
+      data={data}
+      columns={[
+        {
+          Header: intl.formatMessage(messages.headerName),
+          accessor: 'name',
+          // eslint-disable-next-line react/no-unstable-nested-components
+          Cell: ({ row }: CellValue) => (
+            <TableName
+              key={`description-view-${row.original.code}`}
+              name={row.original.name}
+              destination={row.original.homepage}
+              image={row.original.logo}
+            />
+          ),
+        },
+        {
+          Header: intl.formatMessage(messages.headerCourses),
+          accessor: 'courses',
+          Cell: ({ row }: CellValue) => (
+            <>{row.original.courses.length}</>
+          ),
+        },
+        {
+          Header: intl.formatMessage(messages.headerEnrollments),
+          accessor: 'userLimit',
+        },
+        {
+          Header: intl.formatMessage(messages.headerCertified),
+          accessor: 'certified',
+        },
+        {
+          Header: intl.formatMessage(messages.headerCompletion),
+          accessor: 'completion',
+        },
+      ]}
+    >
+      <DataTable.TableControlBar />
+      <DataTable.Table />
+      <TableFooter />
+    </DataTable>
+  );
+};
+
+export default CatalogsList;
