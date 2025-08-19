74dbcf1c30632dc27b81348ecbb10ec21694c20c
Brayan Ceron <brayan.ceron@edunext.co>
2025-08-19 15:29:18 -0500

    feat: add CorporateDetails type and update API to fetch partner details

--- src/app/types.ts
+++ src/app/types.ts
@@ -39,3 +39,13 @@ export type CorporateCatalog = {
   additionalAuthorizationMessage: string;
   courses: CorporateCourse[];
 };
+
+export type CorporateDetails = {
+  name: string;
+  description?: string;
+  image: string;
+  catalogsQuantity: number;
+  coursesQuantity: number;
+  enrollmentsQuantity: number;
+  certifiedLearnersQuantity: number;
+};
