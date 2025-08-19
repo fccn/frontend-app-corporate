4a3beed2f1f8120dacb446961b93420cc1e1ec61
Brayan Ceron <brayan.ceron@edunext.co>
2025-08-19 15:29:06 -0500

    feat: add HeaderDescription & integrate it in the page

---
+++ src/app/HeaderDescription.tsx
@@ -0,0 +1,60 @@
+import {
+  breakpoints, Stack, useMediaQuery,
+} from '@openedx/paragon';
+import React, { FC } from 'react';
+import ImageWithSkeleton from './ImageWithSkeleton';
+
+interface HeaderDescriptionProps {
+  context: {
+    title: string;
+    imageUrl: string;
+    description?: string;
+  },
+  info: {
+    title: string;
+    value: string | number;
+  }[],
+  children?: React.ReactNode;
+}
+
+const HeaderDescription: FC<HeaderDescriptionProps> = ({ context, info, children }) => {
+  const isSmall = useMediaQuery({ maxWidth: breakpoints.small.maxWidth });
+  const isMedium = useMediaQuery({ maxWidth: breakpoints.medium.maxWidth });
+
+  return (
+    <Stack
+      className={`border rounded bg-primary-100 px-${isSmall ? 3 : 4} py-3 my-3 justify-content-between`}
+      direction={isMedium ? 'vertical' : 'horizontal'}
+      gap={4}
+    >
+      <Stack direction={isSmall ? 'vertical' : 'horizontal'} gap={3}>
+        <ImageWithSkeleton
+          src={context.imageUrl}
+          width={150}
+          height={70}
+        />
+
+        <Stack className="justify-content-center">
+          <h3 className="mb-0">{context.title}</h3>
+          <span className="small">{context?.description}</span>
+        </Stack>
+      </Stack>
+
+      <Stack direction="horizontal" gap={4}>
+        {info.map((item, index) => (
+          <>
+            <div key={item.title} className="d-flex flex-column">
+              <span className="x-small text-primary-400">{item.title}</span>
+              <span className="small text-primary-300">{item.value}</span>
+            </div>
+            {index < info.length - 1 && !isSmall && <hr className="border border-left-1 h-25 m-0" />}
+          </>
+        ))}
+      </Stack>
+
+      {children}
+    </Stack>
+  );
+};
+
+export default HeaderDescription;
--- src/catalogs/components/CatalogsList.tsx
+++ src/catalogs/components/CatalogsList.tsx
@@ -1,18 +1,18 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { useIntl } from '@edx/frontend-platform/i18n';
-import {
-  DataTable, TextFilter,
-} from '@openedx/paragon';
+import { DataTable, TextFilter } from '@openedx/paragon';
 
 import { CorporatePartner } from '@src/app/types';
 import TableName from '@src/app/TableName';
 import { useNavigate } from '@src/hooks';
 import { paths } from '@src/constants';
+import HeaderDescription from '@src/app/HeaderDescription';
+import TableFooter from '@src/app/TableFooter';
+import ActionItem from '@src/app/ActionItem';
+
 import { getPartnerCatalogs } from '../api';
-import TableFooter from '../../app/TableFooter';
 
 import messages from '../messages';
-import ActionItem from '../../app/ActionItem';
 
 type CellValue = {
   row: {
@@ -23,6 +23,7 @@ type CellValue = {
 const CatalogsList = () => {
   const navigate = useNavigate();
   const intl = useIntl();
+
   const { data, isLoading } = useSuspenseQuery({
     queryKey: ['partnerCatalogs'],
     queryFn: () => getPartnerCatalogs(),
@@ -37,69 +38,85 @@ const CatalogsList = () => {
   }];
 
   return (
-    <DataTable
-      isLoading={isLoading}
-      isPaginated
-      isFilterable
-      defaultColumnValues={{ Filter: TextFilter }}
-      initialState={{
-        pageSize: 30,
-        pageIndex: 0,
-      }}
-      additionalColumns={[
-        {
-          id: 'action',
-          Header: intl.formatMessage(messages.headerAction),
-          Cell: ({ row }: CellValue) => tableActions.map(({ type, onClick }) => (
-            <ActionItem
-              key={`action-${type}-${row.original.code}`}
-              type={type}
-              onClick={() => onClick(row.original)}
-            />
-          )),
-        },
-      ]}
-      itemCount={data.length}
-      data={data}
-      columns={[
-        {
-          Header: intl.formatMessage(messages.headerName),
-          accessor: 'name',
-          // eslint-disable-next-line react/no-unstable-nested-components
-          Cell: ({ row }: CellValue) => (
-            <TableName
-              key={`description-view-${row.original.code}`}
-              name={row.original.name}
-              destination={row.original.homepage}
-              image={row.original.logo}
-            />
-          ),
-        },
-        {
-          Header: intl.formatMessage(messages.headerCourses),
-          accessor: 'courses',
-          Cell: ({ row }: CellValue) => (
-            <>{row.original.courses.length}</>
-          ),
-        },
-        {
-          Header: intl.formatMessage(messages.headerEnrollments),
-          accessor: 'userLimit',
-        },
-        {
-          Header: intl.formatMessage(messages.headerCertified),
-          accessor: 'certified',
-        },
-        {
-          Header: intl.formatMessage(messages.headerCompletion),
-          accessor: 'completion',
-        },
-      ]}
-    >
-      <DataTable.TableControlBar />
-      <DataTable.Table />
-      <TableFooter />
-    </DataTable>
+    <>
+      <HeaderDescription
+        context={{
+          title: 'Centro Nacional de Cibersegurança PORTUGAL',
+          imageUrl: 'https://www.uc.pt/site/assets/files/545679/cncs_2.1685x774-cropx1314-is.1082x0-is-pid619177.jpg',
+        //   description: 'Manage your catalogs effectively.',
+        }}
+        info={[
+          { title: 'Catalogs', value: 14 },
+          { title: 'Courses', value: 120 },
+          { title: 'Enrollment', value: 2503 },
+          { title: 'Certified Learners', value: 260 },
+        ]}
+      />
+
+      <DataTable
+        isLoading={isLoading}
+        isPaginated
+        isFilterable
+        defaultColumnValues={{ Filter: TextFilter }}
+        initialState={{
+          pageSize: 30,
+          pageIndex: 0,
+        }}
+        additionalColumns={[
+          {
+            id: 'action',
+            Header: intl.formatMessage(messages.headerAction),
+            Cell: ({ row }: CellValue) => tableActions.map(({ type, onClick }) => (
+              <ActionItem
+                key={`action-${type}-${row.original.code}`}
+                type={type}
+                onClick={() => onClick(row.original)}
+              />
+            )),
+          },
+        ]}
+        itemCount={data.length}
+        data={data}
+        columns={[
+          {
+            Header: intl.formatMessage(messages.headerName),
+            accessor: 'name',
+            // eslint-disable-next-line react/no-unstable-nested-components
+            Cell: ({ row }: CellValue) => (
+              <TableName
+                key={`description-view-${row.original.code}`}
+                name={row.original.name}
+                destination={row.original.homepage}
+                image={row.original.logo}
+              />
+            ),
+          },
+          {
+            Header: intl.formatMessage(messages.headerCourses),
+            accessor: 'courses',
+            Cell: ({ row }: CellValue) => (
+              <>{row.original.courses.length}</>
+            ),
+          },
+          {
+            Header: intl.formatMessage(messages.headerEnrollments),
+            accessor: 'userLimit',
+          },
+          {
+            Header: intl.formatMessage(messages.headerCertified),
+            accessor: 'certified',
+          },
+          {
+            Header: intl.formatMessage(messages.headerCompletion),
+            accessor: 'completion',
+          },
+        ]}
+      >
+        <DataTable.TableControlBar />
+        <DataTable.Table />
+        <TableFooter />
+      </DataTable>
+    </>
   );
 };
 
