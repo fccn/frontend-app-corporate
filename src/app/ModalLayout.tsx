948e133297e66366941c0737ab465b5bf02a775d
Brayan Ceron <brayan.ceron@edunext.co>
2025-08-21 16:52:17 -0500

    feat: add ModalLayout component for improved modal dialog handling

---
+++ src/app/ModalLayout.tsx
@@ -0,0 +1,51 @@
+import { ActionRow, ModalDialog } from '@openedx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
+import React, { FC } from 'react';
+import messages from './messages';
+
+interface ModalLayoutProps {
+  title: string;
+  isOpen: boolean;
+  onClose: () => void;
+  actions: React.ReactNode | React.ReactNode[];
+  children: React.ReactNode;
+}
+
+const ModalLayout: FC<ModalLayoutProps> = ({
+  title, isOpen, actions, onClose, children,
+}) => {
+  const intl = useIntl();
+  return (
+    <ModalDialog
+      title={title}
+      isOpen={isOpen}
+      onClose={onClose}
+      size="lg"
+      hasCloseButton
+      isFullscreenOnMobile
+      isOverflowVisible={false}
+    >
+      <ModalDialog.Header className="bg-primary-500">
+        <ModalDialog.Title className="text-light-100">
+          {title}
+        </ModalDialog.Title>
+      </ModalDialog.Header>
+
+      <ModalDialog.Body>
+        {children}
+      </ModalDialog.Body>
+
+      <ModalDialog.Footer>
+        <ActionRow>
+          <ModalDialog.CloseButton variant="outline-primary">
+            {intl.formatMessage(messages.cancelButton)}
+          </ModalDialog.CloseButton>
+
+          {actions}
+        </ActionRow>
+      </ModalDialog.Footer>
+    </ModalDialog>
+  );
+};
+
+export default ModalLayout;
