00e0493fd0f524c26ceb7af07d6fc3696c8ba700
Brayan Ceron <brayan.ceron@edunext.co>
2025-08-29 16:35:57 -0500

    feat: refactor getPartnerCatalogs to improve data structure and error handling

--- src/catalogs/api.ts
+++ src/catalogs/api.ts
@@ -1,25 +1,57 @@
+import { getConfig } from '@edx/frontend-platform';
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 import { logError } from '@edx/frontend-platform/logging';
-import { CorporateCatalog, CorporatePartner } from '../app/types';
+import camelcaseKeys from 'camelcase-keys';
+import { CorporateCatalog, CorporatePartner, Paginated } from '../app/types';
 
-export const getPartnerCatalogs = async (partnerId?: string): Promise<CorporateCatalog[]> => {
+export const getPartnerCatalogs = async (
+  partnerId: string,
+  page: number,
+  pageSize: number,
+): Promise<Paginated<CorporateCatalog>> => {
   try {
-    const response = await getAuthenticatedHttpClient().get(`/api/catalogs${partnerId ? `/${partnerId}` : ''}`);
-    return response.data.catalogs;
+    const url = `${getConfig().LMS_BASE_URL}/corporate_access/api/v1/partners/${partnerId}/catalogs/?page=${page + 1}&page_size=${pageSize}`;
+    const response = await getAuthenticatedHttpClient().get(url);
+    return {
+      next: response.data.next,
+      previous: response.data.previous,
+      count: response.data.count,
+      numPages: response.data.num_pages,
+      currentPage: response.data.current_page,
+      start: response.data.start,
+      results: camelcaseKeys(response.data.results),
+    };
   } catch (error) {
     logError(error);
-    return [];
+    return {
+      next: null,
+      previous: null,
+      count: 0,
+      numPages: 0,
+      currentPage: 0,
+      start: 0,
+      results: [],
+    };
   }
 };
 
 export const getPartnerDetails = async (partnerId?: string): Promise<CorporatePartner> => {
   try {
-    const response = await getAuthenticatedHttpClient().get(`/api/partner${partnerId ? `/${partnerId}` : ''}`);
-    return response.data;
+    const url = `${getConfig().LMS_BASE_URL}/corporate_access/api/v1/partners/${partnerId}/`;
+    const response = await getAuthenticatedHttpClient().get(url);
+    return camelcaseKeys(response.data);
   } catch (error) {
     logError(error);
     return {
-      code: '0', name: '', logo: '', homepage: '', catalogs: 0, courses: 0, enrollments: 0, certified: 0,
+      id: 0,
+      code: '',
+      name: '',
+      logo: '',
+      homepageUrl: '',
+      catalogs: 0,
+      courses: 0,
+      enrollments: 0,
+      certified: 0,
     };
   }
 };
