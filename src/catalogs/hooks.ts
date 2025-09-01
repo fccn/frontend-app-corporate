746449543e2002879193f9b56ce86a3be8ff4e99
Brayan Ceron <brayan.ceron@edunext.co>
2025-09-01 14:13:18 -0500

    feat: add getCatalogDetails API and useCatalogDetails hook for fetching catalog details

--- src/catalogs/hooks.ts
+++ src/catalogs/hooks.ts
@@ -1,11 +1,34 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
-import { getPartnerCatalogs } from './api';
+import { useQuery, useQueryClient, useSuspenseQuery } from '@tanstack/react-query';
+import { CorporateCatalog, PaginatedResponse } from '@src/app/types';
+import { getCatalogDetails, getPartnerCatalogs } from './api';
 
-export const usePartnerCatalogs = ({ partnerId, pageIndex, pageSize }) => {
+export const usePartnerCatalogs = (
+  { partnerId, pageIndex, pageSize }: { partnerId: string; pageIndex: number; pageSize: number; },
+) => {
   const { data: partnerCatalogs, isLoading: isLoadingCatalogs } = useSuspenseQuery({
     queryKey: ['partnerCatalogs', partnerId, pageIndex, pageSize],
     queryFn: () => getPartnerCatalogs(partnerId, pageIndex, pageSize),
   });
 
   return { partnerCatalogs, isLoadingCatalogs };
+};
+
+export const useCatalogDetails = ({ partnerId, selectedCatalog, queryKeyVariables }) => {
+  const queryClient = useQueryClient();
+  const partnerCatalogsCached: PaginatedResponse<CorporateCatalog> | undefined = queryClient.getQueryData(
+    ['partnerCatalogs', partnerId, ...queryKeyVariables],
+  );
+
+  const { data: catalogDetails, isLoading } = useQuery({
+    queryKey: ['catalogDetails'],
+    queryFn: () => getCatalogDetails(partnerId, selectedCatalog),
+    enabled: !partnerCatalogsCached,
+  });
+
+  return {
+    partnerDetails: partnerCatalogsCached
+      ? partnerCatalogsCached.results?.find((catalog) => catalog.id === selectedCatalog)
+      : catalogDetails,
+    isLoadingCatalogDetails: isLoading,
+  };
 };
