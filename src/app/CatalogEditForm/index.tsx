334f457a012ff1ea3f5db83b544e3a85835918e6
Brayan Ceron <brayan.ceron@edunext.co>
2025-09-01 14:25:26 -0500

    refactor: update form field names for consistency in CatalogEditForm

--- src/app/CatalogEditForm/index.tsx
+++ src/app/CatalogEditForm/index.tsx
@@ -14,25 +14,8 @@ const CATALOG_FORM_CONFIG: FormElement[] = [
   { type: 'title', label: 'General Information' },
   { name: 'isPublic', type: 'switch', label: 'Public Catalog' },
   { name: 'name', type: 'text', label: 'Catalog Name' },
-  { name: 'homepage', type: 'text', label: 'Alternative Link' },
+  { name: 'catalogAlternativeLink', type: 'text', label: 'Alternative Link' },
   { name: 'supportEmail', type: 'email', label: 'Support Email' },
-  {
-    type: 'group',
-    fields: [
-      { name: 'startDate', type: 'date', label: 'Start Date' },
-      { name: 'endDate', type: 'date', label: 'End Date' },
-    ],
-  },
-  { type: 'divider' },
-
-  { type: 'title', label: 'Enrollment Settings' },
-  {
-    type: 'group',
-    fields: [
-      { name: 'enrollmentLimit', type: 'number', label: 'Course Enrollment Limit' },
-      { name: 'userLimit', type: 'number', label: 'User Limit' },
-    ],
-  },
   {
     type: 'group',
     fields: [
@@ -42,9 +25,19 @@ const CATALOG_FORM_CONFIG: FormElement[] = [
   },
   { type: 'divider' },
 
+  { type: 'title', label: 'Enrollment Settings' },
+  {
+    type: 'group',
+    fields: [
+      { name: 'courseEnrollmentLimit', type: 'number', label: 'Course Enrollment Limit' },
+      { name: 'userLimit', type: 'number', label: 'User Limit' },
+    ],
+  },
+  { type: 'divider' },
+
   { type: 'title', label: 'Advanced Settings' },
-  { name: 'emailDomainRegex', type: 'multiple-text', label: 'Partner Email Regex' },
-  { name: 'enableCustomCourses', type: 'switch', label: 'Enable Custom Courses' },
+  { name: 'emailRegexes', type: 'multiple-text', label: 'Partner Email Regex' },
+  { name: 'customCourses', type: 'switch', label: 'Enable Custom Courses' },
   { name: 'isSelfEnrollment', type: 'switch', label: 'Self Enrollment' },
 ];
 
