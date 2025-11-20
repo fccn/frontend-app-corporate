6048e07ffb22649275525ebd46c8b579bd317f44
Diana Olarte <diana.olarte@edunext.co>
2025-11-20 20:59:13 +1100

    refactor: avoid the use of contenxt for the settings modal

--- src/catalogs/components/CatalogSettingsModal/index.ts
+++
@@ -1,4 +0,0 @@
-import { CatalogSettingsModalProvider } from './context/CatalogSettingsModalProvider';
-import { useCatalogSettingsModal } from './context/useCatalogSettings';
-
-export { CatalogSettingsModalProvider, useCatalogSettingsModal };
